596f64279c427cbbdcf17c6cb808880f0af5bc2f
kanat <turganbaevkanat88@gmail.com>
2024-03-22 14:01:03 +0300

    count

--- index.js
+++ index.js
@@ -16,15 +16,21 @@ inquirer.prompt([
 ]).then(async (answer) => {
   // Получаем вопросы и ответы из выбранного квиза
   const qa = await getQA(directory, answer.value);
-
+  console.log( answer.value)
+let count = 0
   // Поочередно выводим вопросы и получаем ответы от пользователя
   for (let i = 0; i < qa.length; i++) {
     const question = qa[i][0];
     const correctAnswer = qa[i][1];
-    
     const userAnswer = await inquirer.prompt({ type: 'input', name: 'userAnswer', message: question });
-    
+    if(userAnswer === correctAnswer){
+     count++
+    }else{
+     count
+    }
     console.log('Ваш ответ:', userAnswer.userAnswer);
     console.log('Правильный ответ:', correctAnswer);
+    console.log(count)
+    
   }
 });
