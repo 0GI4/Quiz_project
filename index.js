0e1509cf66d44923c1a9d06adcad7ca08915bd23
0GI4 <pop9991@outlook.com>
2024-03-22 14:01:35 +0300

    addedChalk

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 const fs = require('fs').promises;
 const inquirer = require('inquirer');
+const chalk = require('chalk');
 const { getQA } = require('./readFile');
 
-const packageNew = [{ name: 'Ястребы', value: 0 }, { name: 'Еноты', value: 1 }, { name: 'Выдры', value: 2 }];
-const directory = './topics'; // Директория с файлами квиза
+const packageNew = [
+  { name: 'Ястребы', value: 0 },
+  { name: 'Выдры', value: 1 },
+  { name: 'Еноты', value: 2 },
+];
+const directory = './topics';
 
-inquirer.prompt([
-  { type: 'input', name: 'username', message: 'Введите имя:' },
-  {
-    type: 'list',
-    name: 'value',
-    message: 'Выберите квиз:',
-    choices: packageNew,
-  },
-]).then(async (answer) => {
-  // Получаем вопросы и ответы из выбранного квиза
-  const qa = await getQA(directory, answer.value);
+inquirer
+  .prompt([
+    { type: 'input', name: 'username', message: 'Введите имя:' },
+    {
+      type: 'list',
+      name: 'value',
+      message: 'Выберите квиз:',
+      choices: packageNew,
+    },
+  ])
+  .then(async (answer) => {
+    const qa = await getQA(directory, answer.value);
 
-  // Поочередно выводим вопросы и получаем ответы от пользователя
-  for (let i = 0; i < qa.length; i++) {
-    const question = qa[i][0];
-    const correctAnswer = qa[i][1];
-    
-    const userAnswer = await inquirer.prompt({ type: 'input', name: 'userAnswer', message: question });
-    
-    console.log('Ваш ответ:', userAnswer.userAnswer);
-    console.log('Правильный ответ:', correctAnswer);
-  }
-});
+    for (let i = 0; i < qa.length; i++) {
+      const question = qa[i][0];
+      const correctAnswer = qa[i][1];
+
+      const userAnswer = await inquirer.prompt({
+        type: 'input',
+        name: 'userAnswer',
+        message: question,
+      });
+
+      console.log(chalk.blue('Ваш ответ:', userAnswer.userAnswer));
+      console.log(chalk.red('Правильный ответ:', correctAnswer));
+    }
+    console.log('\nВсе Правильно!');
+  });
