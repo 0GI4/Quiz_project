e95b4a188640f7ddfbd7274a26fd3868f9a54f09
Ivan2599 <evrjmet@yandex.ru>
2024-03-22 13:20:13 +0300

    ReadFileTest end

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const fs = require('fs').promises;
 const inquirer = require('inquirer');
-const readDir  = require('./readDir');
+const { getQA } = require('./readFile');
 
-const packageNew = [{name: 'Ястребы', value:0}, {name: 'Еноты', value:1}, {name: 'Выдры', value:2}]
+const packageNew = [{ name: 'Ястребы', value: 0 }, { name: 'Еноты', value: 1 }, { name: 'Выдры', value: 2 }];
+const directory = './topics'; // Директория с файлами квиза
 
 inquirer.prompt([
   { type: 'input', name: 'username', message: 'Введите имя:' },
   {
     type: 'list',
-
     name: 'value',
-
     message: 'Выберите квиз:',
-
     choices: packageNew,
   },
-]).then((answer)=> readFile(readDir, answer.value)) 
+]).then(async (answer) => {
+  // Получаем вопросы и ответы из выбранного квиза
+  const qa = await getQA(directory, answer.value);
+
+  // Поочередно выводим вопросы и получаем ответы от пользователя
+  for (let i = 0; i < qa.length; i++) {
+    const question = qa[i][0];
+    const correctAnswer = qa[i][1];
+    
+    const userAnswer = await inquirer.prompt({ type: 'input', name: 'userAnswer', message: question });
+    
+    console.log('Ваш ответ:', userAnswer.userAnswer);
+    console.log('Правильный ответ:', correctAnswer);
+  }
+});
